d682ccb9af36c12353633b5ed97d62db756252b7
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2024-05-17 14:17:11 +0200

    Add population of sidebar date and weather

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@
 
 import './style.css';
 import html from './index.html';
+import { showDay } from './modules/weatherInfo';
+import { fetchWeather, showWeather } from './modules/weatherInfo';
 
 // Set copyright year automatically
 const copyrightSpan = document.querySelector('#copyright-span');
 copyrightSpan.textContent = new Date().getFullYear();
-console.log(copyrightSpan);
+
+// Set day and weather in sidebar header
+window.onload = async () => {
+  showDay();
+  const getWeather = await fetchWeather();
+  showWeather(getWeather);
+};
