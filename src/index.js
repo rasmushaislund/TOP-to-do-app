63eb5e5265dd652a688f560d7ae3baf8c6638131
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2024-05-22 12:15:31 +0200

    Add eventlisteners for dropdown menu and project name update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './style.css';
 import html from './index.html';
 import { showDay } from './modules/weatherInfo';
 import { fetchWeather, showWeather } from './modules/weatherInfo';
+import toggleDropdown from './modules/editProject';
+import { selectProject } from './modules/selectProject';
 
 // Set copyright year automatically
 const copyrightSpan = document.querySelector('#copyright-span');
@@ -15,3 +17,27 @@ window.onload = async () => {
   const getWeather = await fetchWeather();
   showWeather(getWeather);
 };
+
+// Toggle project menu dropdown (three horizontal dots)
+const dropdownBtn = document.querySelector('.three-dots-btn');
+const dropdownMenu = document.querySelector('.dropdown');
+
+dropdownBtn.addEventListener('click', (e) => {
+  e.stopPropagation();
+  toggleDropdown();
+});
+
+document.documentElement.addEventListener('click', () => {
+  if (dropdownMenu.classList.contains('show')) {
+    toggleDropdown();
+  }
+});
+
+// Select project and update header project title
+const sidebarItem = document.querySelectorAll('.sidebar-item');
+
+sidebarItem.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    selectProject(e);
+  });
+});
