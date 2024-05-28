3f8b730116c315190b1be1d6a33d9f2784a0c981
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2024-05-28 09:37:44 +0200

    Hide new task card when settings or help is chosen

--- src/modules/selectProject.js
+++ src/modules/selectProject.js
@@ -1,9 +1,17 @@
 export function selectProject(e) {
-  const parentTarget = e.target.closest('.sidebar-item');
-  const targetChild = parentTarget.querySelector('.item-text');
-  const childText = targetChild.textContent;
+  if (e.target.closest('.selectable-item')) {
+    const parentTarget = e.target.closest('.selectable-item');
+    const targetChild = parentTarget.querySelector('.item-text');
+    const childText = targetChild.textContent;
+    const newTask = document.querySelector('.new-task');
 
-  setProjectText(childText);
+    newTask.classList.remove('hide');
+    setProjectText(childText);
+
+    if (e.target.closest('.fixed-item')) {
+      newTask.classList.add('hide');
+    }
+  }
 }
 
 const setProjectText = (text) => {
