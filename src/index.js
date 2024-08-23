52580e91c02b5bafacbf97e78209d6ad123cc2a1
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2024-08-23 21:40:53 +0200

    Update import and create handler for new project creation

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@
 
 import './style.css';
 import html from './index.html';
-import { showDay } from './modules/weatherInfo';
-import { fetchWeather, showWeather } from './modules/weatherInfo';
-import toggleDropdown from './modules/editProject';
-import { selectProject } from './modules/selectProject';
-import { expandCollapse } from './modules/taskCard';
+import { showDay } from './dom/weatherInfo';
+import { fetchWeather, showWeather } from './dom/weatherInfo';
+import toggleDropdown from './dom/editProject';
+import { selectProject } from './dom/selectProject';
+import { expandCollapse } from './dom/taskCard';
+import { domHandler } from './handler/domHandler';
 
 // Set copyright year automatically
 const copyrightSpan = document.querySelector('#copyright-span');
@@ -35,12 +36,15 @@ document.documentElement.addEventListener('click', () => {
 });
 
 // Select project and update header project title
-const sidebarItem = document.querySelectorAll('.sidebar-item');
+const sidebarTop = document.querySelector('.sidebar-top');
+const sidebarSettings = document.querySelector('.sidebar-settings');
 
-sidebarItem.forEach((item) => {
-  item.addEventListener('click', (e) => {
-    selectProject(e);
-  });
+sidebarTop.addEventListener('click', (e) => {
+  selectProject(e);
+});
+
+sidebarSettings.addEventListener('click', (e) => {
+  selectProject(e);
 });
 
 // Task card
@@ -50,3 +54,26 @@ const expandCollapseBtn = document.querySelector('#expand-collapse-btn');
 expandCollapseBtn.addEventListener('click', () => {
   expandCollapse();
 });
+
+// Add new project by calling handler on click
+const addProjectBtn = document.querySelector('#add-project-btn');
+const addProjectTitle = document.querySelector('#add-project');
+
+addProjectBtn.addEventListener('click', () => {
+  domHandler(addProjectTitle.value);
+});
+
+// ... or press 'enter'
+addProjectTitle.addEventListener('keypress', (e) => {
+  if (e.keyCode === 13) {
+    addProjectBtn.click();
+  }
+});
+
+// Add new task by calling handler on click
+const addTaskBtn = document.querySelector('#add-task-btn');
+const addTaskTitle = document.querySelector('.task-title');
+const addTaskDeadline = document.querySelector('.deadline-para');
+const addTaskDescription = document.querySelector('.task-description');
+
+addTaskBtn.addEventListener('click', () => {});
